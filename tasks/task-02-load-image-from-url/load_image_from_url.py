4f833b795354ace38171c36548e87e35e104d50d
tvieira <tvieira@ic.ufal.br>
2025-02-19 09:30:23 -0300

    Added task-02, task-03 and ch02 slides

---
+++ tasks/task-02-load-image-from-url/load_image_from_url.py
@@ -0,0 +1,23 @@
+import argparse
+import numpy as np
+import cv2 as cv
+
+def load_image_from_url(url, **kwargs):
+    """
+    Loads an image from an Internet URL with optional arguments for OpenCV's cv.imdecode.
+    
+    Parameters:
+    - url (str): URL of the image.
+    - **kwargs: Additional keyword arguments for cv.imdecode (e.g., flags=cv.IMREAD_GRAYSCALE).
+    
+    Returns:
+    - image: Loaded image as a NumPy array.
+    """
+    
+    ### START CODE HERE ###
+    ### TODO
+    ### END CODE HERE ###
+    
+    return image
+
+load_image_from_url()
---
+++ tasks/task-03-get-image-info/task-03-get_image_info.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def get_image_info(image):
+    """
+    Extracts metadata and statistical information from an image.
+
+    Parameters:
+    - image (numpy.ndarray): Input image.
+
+    Returns:
+    - dict: Dictionary containing image metadata and statistics.
+    """
+    
+    ### START CODE HERE ###
+    ### TODO
+    ### END CODE HERE ###
+
+    return {
+        "width": width,
+        "height": height,
+        "dtype": dtype,
+        "depth": depth,
+        "min_value": min_val,
+        "max_value": max_val,
+        "mean": mean_val,
+        "std_dev": std_val
+    }
+
+# Example Usage:
+sample_image = np.random.randint(0, 256, (100, 100), dtype=np.uint8)
+info = get_image_info(sample_image)
+
+# Print results
+for key, value in info.items():
+    print(f"{key}: {value}")
