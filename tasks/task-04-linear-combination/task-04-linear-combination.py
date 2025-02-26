2cae625eb34797ffa9fbe5467ce81dd1c56c0027
tiagoccs97 <39594949+tiagoccs97@users.noreply.github.com>
2025-02-26 20:26:09 -0300

    completed code function

--- tasks/task-04-linear-combination/task-04-linear-combination.py
+++ tasks/task-04-linear-combination/task-04-linear-combination.py
@@ -20,7 +20,19 @@ def linear_combination(i1: np.ndarray, i2: np.ndarray, a1: float, a2: float) -> 
         raise ValueError("Input images must have the same dimensions.")
 
     ### START CODE HERE ###
-    ### TODO
+    if i1 is None:
+        print("Error loading i1")
+        exit(-1)
+    elif i2 is None:
+        print("Error loading i2")
+        exit(-1)
+    # Blending images
+    bld = cv2.addWeighted(i1, a1, i2, a2, 0.0)
+    # Display
+    cv2.imshow('Blended Images', bld)
+    cv2.waitKey(0)
+    #Display Clear
+    cv2.destroyAllWindows()
     ### END CODE HERE ###
 
     return None
@@ -28,8 +40,8 @@ def linear_combination(i1: np.ndarray, i2: np.ndarray, a1: float, a2: float) -> 
 # Example Usage
 if __name__ == "__main__":
     # Load images
-    i1 = cv2.imread('image1.jpg', cv2.IMREAD_COLOR)
-    i2 = cv2.imread('image2.jpg', cv2.IMREAD_COLOR)
+    i1 = cv2.imread('image1.png', cv2.IMREAD_COLOR)
+    i2 = cv2.imread('image2.png', cv2.IMREAD_COLOR)
 
     if i1 is None or i2 is None:
         raise FileNotFoundError("One or both images could not be loaded. Check file paths.")
@@ -39,3 +51,4 @@ if __name__ == "__main__":
 
     # Compute the linear combination
     output = linear_combination(i1, i2, a1, a2)
+    
