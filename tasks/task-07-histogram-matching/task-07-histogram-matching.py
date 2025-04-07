1e31b6d206865e05601c1c0f33328d26e2d79732
tiagoccs97 <39594949+tiagoccs97@users.noreply.github.com>
2025-04-06 23:38:29 -0300

    Update task-07-histogram-matching.py

--- tasks/task-07-histogram-matching/task-07-histogram-matching.py
+++ tasks/task-07-histogram-matching/task-07-histogram-matching.py
@@ -26,8 +26,32 @@ Notes:
 
 import cv2 as cv
 import numpy as np
-import scikitimage as ski
+#import scikitimage as ski
 
 def match_histograms_rgb(source_img: np.ndarray, reference_img: np.ndarray) -> np.ndarray:
     # Your implementation here
-    pass
+    matched_img = np.zeros_like(source_img)
+    
+    for canal in range(3):  #R, G, B
+
+        fonte = source_img[:,:,canal]
+        ref = reference_img[:,:,canal]
+        
+        hist_fonte = np.histogram(fonte, bins=256, range=(0,255))[0]
+        hist_ref = np.histogram(ref, bins=256, range=(0,255))[0]
+        
+        print("fonte\n", fonte, "\n", "hist_fonte\n", hist_fonte)
+        
+        cdf_fonte = hist_fonte.cumsum()
+        cdf_fonte = cdf_fonte / cdf_fonte[-1]
+        
+        cdf_ref = hist_ref.cumsum()
+        cdf_ref = cdf_ref / cdf_ref[-1]
+        
+        mapeamento = np.zeros(256, dtype=np.uint8)
+        for v in range(256):
+            mapeamento[v] = np.argmin(np.abs(cdf_ref - cdf_fonte[v]))
+        
+        matched_img[:,:,canal] = mapeamento[fonte]
+    
+    return matched_img
